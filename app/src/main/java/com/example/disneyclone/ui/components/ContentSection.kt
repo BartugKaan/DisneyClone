0aaffc51bca913b01ece5711aab7a9aefbfb5466
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-24 13:42:53 +0300

    bugfix: deleted import

--- app/src/main/java/com/example/disneyclone/ui/components/ContentSection.kt
+++ app/src/main/java/com/example/disneyclone/ui/components/ContentSection.kt
@@ -11,7 +11,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.disneyclone.ui.screens.ContentCard
 import com.example.disneyclone.ui.theme.DisneyWhite
 
 
