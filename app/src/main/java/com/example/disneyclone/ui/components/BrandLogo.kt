904cddc3111ea2346ac3f6c3bbf952fdd1e994f0
Bartuğ Kaan  Çelebi <bartugkaan.celebi@outlook.com>
2024-12-24 15:16:41 +0300

    update: border color for BrandLogo component

--- app/src/main/java/com/example/disneyclone/ui/components/BrandLogo.kt
+++ app/src/main/java/com/example/disneyclone/ui/components/BrandLogo.kt
@@ -2,14 +2,14 @@ package com.example.disneyclone.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
@@ -21,9 +21,13 @@ fun BrandLogo(resourceId: Int) {
         modifier = Modifier
             .size(width = 65.dp, height = 45.dp)
             .clip(RoundedCornerShape(5.dp))
-            .background(DisneySecondary),
+            .background(DisneySecondary)
+            .border(
+                width = 0.5.dp,
+                color = Color.White.copy(alpha = 0.2f),
+                shape = RoundedCornerShape(5.dp)
+            ),
         contentAlignment = Alignment.Center
-
     ) {
         Image(
             painter = painterResource(id = resourceId),
